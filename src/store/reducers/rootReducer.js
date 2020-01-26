ece0c7f9ce5578d6782e8dd16393e816f8aa2486
Marta Farre <martafarreribas@gmail.com>
2020-01-26 19:35:04 +0700

    JSON data sorted for initial state

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,9 +1,25 @@
 import { combineReducers } from 'redux';
 import data from '../data/host-app-data.json';
+import { mergeSort } from '../../utils';
 
-const appHostData = () =>  data;
+const hostData = () => {
+    const hostsList = {};
+    for (let app of data) {
+        for (let host of app.host) {
+            if(hostsList[host]) {
+                hostsList[host].push(app);
+            } else {
+                hostsList[host] = [app];
+            }
+        };
+    };
+    for (let host in hostsList) {        
+        hostsList[host] = mergeSort(hostsList[host]);
+    }
+    return hostsList;
+}
 
 export default combineReducers({
-  	appHostData
+  	hostData
 });
 
