b15acd5047a19af01a7257eb7757223c7406b9b6
Marta Farre <martafarreribas@gmail.com>
2020-01-30 13:57:03 +0700

    Testing layout reducer

---
+++ src/store/reducers/layoutReducer.test.js
@@ -0,0 +1,19 @@
+import layoutReducer from './layoutReducer';
+import { CHANGE_LAYOUT } from '../actions/constants';
+
+describe('Layout reducer', () => {
+    const isAwesomeGrid = false;
+    
+    it('should return default state', () => {
+        const newState = layoutReducer(isAwesomeGrid, {});
+        expect(newState).toEqual(isAwesomeGrid)
+    });
+
+    it('should return new action.payload if CHANGE_LAYOUT type', () => {
+        const newStateProperty = layoutReducer(undefined, {
+            type: CHANGE_LAYOUT,
+            payload: true
+        });
+        expect(newStateProperty).toEqual(true)
+    });
+});
